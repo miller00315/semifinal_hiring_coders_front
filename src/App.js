a55351bea556d21ea793bceb6b1c2129b83c69c0
Mara <mara.15oliveira@outlook.com>
2020-09-08 10:47:57 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbot from './components/chatbot/Chatbot'
+import './App.css';
+
+function App() {
+  return (
+    <div className='bot'>
+      <Chatbot/>
+    </div>
+  );
+}
+
+export default App;
