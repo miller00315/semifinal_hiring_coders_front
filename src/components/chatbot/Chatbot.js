96375a34ec3418022203dad0737b12b004266c6c
Miller Oliveira <mcesar@ocatec.com.br>
2020-09-13 15:30:56 -0300

    Front integrado com back

--- src/components/chatbot/Chatbot.js
+++ src/components/chatbot/Chatbot.js
@@ -1,80 +1,176 @@
-import React, { useState } from 'react';
-import './Chatbot.css'
-import {FiSend} from 'react-icons/fi'
-//import dialogo from './../../dialogo.json'
-import api from '../../api'
+import React, { useMemo, useState, useEffect } from 'react';
+import './Chatbot.css';
+import { FiSend } from 'react-icons/fi';
+import { useQuery } from 'react-apollo';
+import gql from 'graphql-tag';
+import { v4 as uuidV4 } from 'uuid';
 
-
-const Chatbot = () =>{
-
-    const [chatOpen, setChatOpen] = useState(false);
-    const [dialogo, setDialogo] = useState([
-        {
-            text: 'Olá, bem vindo ao chatbot!',
-            user: 'assistente',
-        },
-        {
-            text: 'Gostaria de comprar um notebook.',
-            user: 'cliente',
+const CHAT = gql`
+  query CHAT(
+    $userId: String!
+    $userName: String!
+    $content: String!
+    $id: String!
+  ) {
+    chat(
+      options: {
+        user: { id: $userId, name: $userName }
+        content: $content
+        id: $id
+      }
+    ) {
+      items {
+        id
+        content
+        user {
+          id
+          name
         }
-    ]);
-
-    const[formData,setFormData] = useState({
-        question: '',
-        user: 'cliente',
-    })
-    
-    function sayHello(e){
-        e.preventDefault();
-        setChatOpen(!chatOpen)
-        chatOpen ? document.getElementById('chatbot').classList.add("newClass") : document.getElementById('chatbot').classList.remove("newClass");
+      }
+      totalItems
     }
+  }
+`;
 
-    function handleInputChange(e){
-        const {name, value}= e.target;
-        setFormData({...formData, [name]:value});
-    }
+const initialCode = uuidV4();
 
-    async function handleSubmit(e){
-        e.preventDefault();
+const Chatbot = () => {
+  const initial = {
+    user: { id: 'ghhgjg', name: 'Miller César' },
+    content: 'initial',
+  };
 
-        const {name, value} = e.target;
+  const { data, error, fetchMore } = useQuery(CHAT, {
+    variables: {
+      userId: initial.user.id,
+      userName: initial.user.name,
+      content: initial.content,
+      id: initialCode,
+    },
+    skip: !initial,
+    fetchPolicy: 'cache-first',
+  });
 
-        
-        const data = [{[name]:value}]
-        console.log(data);
-        const resp = await api.post('api/botStore/userQuestion', data)
+  const initialValues = useMemo(
+    () => ({
+      messages: data?.chat.items ?? [],
+      totalItems: data?.chat.totalItems ?? 0,
+    }),
+    [data]
+  );
 
-        setDialogo(resp)
-    }
+  const [values, setValues] = useState(initialValues);
 
-    return(
-        <>
-        <button id='button-open-chat' className='button-open-chat' onClick={sayHello}>
-            <img src="https://icon-library.net//images/bot-icon/bot-icon-7.jpg" width="90" />
-        </button>
+  useEffect(() => setValues(initialValues), [initialValues]);
 
-        <div id='chatbot' className='chatbot' >
-            <div className='chat-header'> Chatbot </div>
+  const [chatOpen, setChatOpen] = useState(false);
 
-            <div className='chat-dialogo'>
-                {dialogo.map(item=>(
-                    <>
-                        <p key={item.id} className='dialogo-text' >{item.text}</p>
-                        <small className='dialogo-user'>{item.user}</small>
-                    </>
-                ))} 
+  const [formData, setFormData] = useState({
+    question: '',
+    user: initial.user,
+  });
+
+  function sayHello(e) {
+    e.preventDefault();
+    setChatOpen(!chatOpen);
+    chatOpen
+      ? document.getElementById('chatbot').classList.add('newClass')
+      : document.getElementById('chatbot').classList.remove('newClass');
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const message = {
+      id: uuidV4(),
+      content: formData.question,
+      user: initial.user,
+    };
+
+    const messages = values.messages;
+
+    messages.push(message);
+
+    setFormData({
+      question: '',
+    });
+
+    setValues({
+      messages,
+    });
+
+    fetchMore({
+      variables: {
+        userId: message.user.id,
+        userName: message.user.name,
+        content: message.id,
+        messageId: message.content,
+      },
+      updateQuery: (result, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          setValues({
+            messages: values.messages.concat(result.chat.items),
+          });
+        } else {
+          setValues({
+            messages: values.messages.concat(fetchMoreResult.chat.items),
+          });
+        }
+      },
+    });
+  }
+
+  return (
+    <>
+      <button
+        id="button-open-chat"
+        className="button-open-chat"
+        onClick={sayHello}
+      >
+        <img
+          src="https://icon-library.net//images/bot-icon/bot-icon-7.jpg"
+          width="90"
+          alt="Imagem do chat"
+        />
+      </button>
+
+      <div id="chatbot" className="chatbot">
+        <div className="chat-header"> Chatbot </div>
+
+        <div className="chat-dialogo">
+          {values.messages.map((item) => (
+            <div key={item.id}>
+              <p key={item.id} className="dialogo-text">
+                {item.content}
+              </p>
+              <small className="dialogo-user">{item.user.name}</small>
             </div>
+          ))}
+        </div>
 
-            <div className='chat-input' >
-                <form onSubmit={handleSubmit} className='formulario'>
-                    <input type="text" name="question" class="form-control" id="" onChange={handleInputChange} />
-                    <button type="submit" class="btn btn-primary"><FiSend/></button>
-                </form>
-            </div>
-        </div> 
-        </>
-    )
-}
+        <div className="chat-input">
+          <form onSubmit={handleSubmit} className="formulario">
+            <input
+              type="text"
+              name="question"
+              className="form-control"
+              id="chatInput"
+              onChange={handleInputChange}
+              value={formData.question}
+            />
+            <button type="submit" className="btn btn-primary">
+              <FiSend />
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Chatbot;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloProvider } from 'react-apollo';
+
+import client from './plugins/apollo/client';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
